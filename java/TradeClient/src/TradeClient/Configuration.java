e35aea09e79612a9faee2f97cfac9a6f446b4391
dbrown <dbrown@Marvin.redpine.local>
2015-06-03 07:05:30 -0400

    On branch master 	new file:   java/TradeClient/.classpath 	new file:   java/TradeClient/.gitignore 	new file:   java/TradeClient/.project 	new file:   java/TradeClient/.settings/org.eclipse.jdt.core.prefs 	new file:   java/TradeClient/.settings/org.eclipse.jdt.launching.prefs 	new file:   java/TradeClient/.settings/org.eclipse.m2e.core.prefs 	new file:   java/TradeClient/pom.xml 	new file:   java/TradeClient/src/TradeClient/Configuration.java 	new file:   java/TradeClient/src/TradeClient/TickData.java 	new file:   java/TradeClient/src/TradeClient/TradeClient.java 	new file:   java/TradeClient/src/TradeMessageConsumer/L1Data.java 	new file:   java/TradeClient/src/TradeMessageConsumer/L1DataObserver.java 	new file:   java/TradeClient/src/TradeMessageConsumer/TradeMessageConsumer.java

---
+++ java/TradeClient/src/TradeClient/Configuration.java
@@ -0,0 +1,20 @@
+package TradeClient;
+
+public class Configuration
+{
+    private static final Configuration instance_ = new Configuration();
+
+    public static Configuration getInstance()
+    {
+	return instance_;
+    }
+    
+    String[] getSymbols()
+    {
+	String [] ret = {"EUR/USD","EUR/CHF","USD/CHF"};
+	return ret;
+    }
+    private Configuration()
+    {
+    }
+}
---
+++ java/TradeClient/src/TradeMessageConsumer/L1DataObserver.java
@@ -0,0 +1,7 @@
+package TradeMessageConsumer;
+
+public interface L1DataObserver
+{
+    public void update(L1Data data);
+
+}
