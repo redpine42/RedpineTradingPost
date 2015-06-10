ce08f375a36182c2842b06bdfba05ff4969d210d
dbrown <dbrown@Marvin.redpine.local>
2015-06-10 06:29:19 -0400

    Inital unit tests

---
+++ java/TradeDataAccess/src/test/java/com/redpine/TradeDataAccess/Data/L1dataTest.java
@@ -0,0 +1,240 @@
+/**
+ * 
+ */
+package com.redpine.TradeDataAccess.Data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dbrown
+ *
+ */
+public class L1dataTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#L1data()}.
+	 */
+	@Test
+	public void testL1data() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#L1data(java.lang.String, int, double, int, int, double, double, int, int, int)}.
+	 */
+	@Test
+	public void testL1dataStringIntDoubleIntIntDoubleDoubleIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getSeq()}.
+	 */
+	@Test
+	public void testGetSeq() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setSeq(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetSeq() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getTimeStamp()}.
+	 */
+	@Test
+	public void testGetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setTimeStamp(float)}.
+	 */
+	@Test
+	public void testSetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getSymbol()}.
+	 */
+	@Test
+	public void testGetSymbol() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setSymbol(java.lang.String)}.
+	 */
+	@Test
+	public void testSetSymbol() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getSize()}.
+	 */
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setSize(int)}.
+	 */
+	@Test
+	public void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getPrice()}.
+	 */
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setPrice(double)}.
+	 */
+	@Test
+	public void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getTime()}.
+	 */
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setTime(int)}.
+	 */
+	@Test
+	public void testSetTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getJdate()}.
+	 */
+	@Test
+	public void testGetJdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setJdate(int)}.
+	 */
+	@Test
+	public void testSetJdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getBid()}.
+	 */
+	@Test
+	public void testGetBid() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setBid(double)}.
+	 */
+	@Test
+	public void testSetBid() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getAsk()}.
+	 */
+	@Test
+	public void testGetAsk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setAsk(double)}.
+	 */
+	@Test
+	public void testSetAsk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getBidsize()}.
+	 */
+	@Test
+	public void testGetBidsize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setBidsize(int)}.
+	 */
+	@Test
+	public void testSetBidsize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getAsksize()}.
+	 */
+	@Test
+	public void testGetAsksize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setAsksize(int)}.
+	 */
+	@Test
+	public void testSetAsksize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#getVolume()}.
+	 */
+	@Test
+	public void testGetVolume() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.redpine.TradeDataAccess.Data.L1data#setVolume(int)}.
+	 */
+	@Test
+	public void testSetVolume() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ java/TradeDataAccess/src/test/java/com/redpine/TradeDataAccess/Data/L2dataTest.java
@@ -0,0 +1,139 @@
+package com.redpine.TradeDataAccess.Data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class L2dataTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testL2data() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testL2dataStringStringStringByteDoubleIntIntIntByte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSeq() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSeq() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSymbol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSymbol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMmid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMmid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketside() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketside() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClosed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetClosed() {
+		fail("Not yet implemented");
+	}
+
+}
