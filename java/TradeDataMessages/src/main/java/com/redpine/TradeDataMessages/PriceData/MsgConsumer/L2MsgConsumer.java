e500fea78a9e47cc15f89b6f03bd8cd6b3328c8d
dbrown <dbrown@Marvin.redpine.local>
2015-07-12 07:53:28 -0400

    Barebones price data jms messaging consumers

--- java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/MsgConsumer/L2MsgConsumer.java
+++ java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/MsgConsumer/L2MsgConsumer.java
@@ -1,5 +1,27 @@
 package com.redpine.TradeDataMessages.PriceData.MsgConsumer;
 
-public class L2MsgConsumer {
+import javax.jms.JMSException;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class L2MsgConsumer implements MessageListener {
+	private static final Logger log = LoggerFactory
+			.getLogger(L2MsgConsumer.class);
+
+	public void onMessage(javax.jms.Message message) {
+		if (message instanceof TextMessage) {
+			try {
+				log.info("Recieved L2 message: "
+						+ ((TextMessage) message).getText());
+			} catch (JMSException ex) {
+				throw new RuntimeException(ex);
+			}
+		} else {
+			throw new IllegalArgumentException(
+					"Message must be of type TextMessage");
+		}
+	}
 }
--- java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/MsgConsumer/TimeSalesMsgConsumer.java
+++ java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/MsgConsumer/TimeSalesMsgConsumer.java
@@ -1,5 +1,27 @@
 package com.redpine.TradeDataMessages.PriceData.MsgConsumer;
 
-public class TimeSalesMsgConsumer {
+import javax.jms.JMSException;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimeSalesMsgConsumer implements MessageListener {
+	private static final Logger log = LoggerFactory
+			.getLogger(TimeSalesMsgConsumer.class);
+
+	public void onMessage(javax.jms.Message message) {
+		if (message instanceof TextMessage) {
+			try {
+				log.info("Recieved TimeSales message: "
+						+ ((TextMessage) message).getText());
+			} catch (JMSException ex) {
+				throw new RuntimeException(ex);
+			}
+		} else {
+			throw new IllegalArgumentException(
+					"Message must be of type TextMessage");
+		}
+	}
 }
