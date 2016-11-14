700f7933b3f06f00f8c62a72bc0f1b8a360a6ab7
D Brown <danrb@redpine.com>
2016-11-14 11:59:27 -0500

    Updates to readme and other code
    Updates to readme and other code

--- java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/KafkaConsumer/KafkaConfig.java
+++ java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/KafkaConsumer/KafkaConfig.java
@@ -1,27 +1,41 @@
+package com.redpine.TradeDataMessages.PriceData.KafkaConsumer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+
 @Configuration
 @EnableKafka
 public class KafkaConfig {
 
-@Bean
-public KafkaListenerContainerFactory<?> kafkaJsonListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory());
-    factory.setMessageConverter(new StringJsonMessageConverter());
-    return factory;
-}
+	@Bean
+	public KafkaListenerContainerFactory<?> kafkaJsonListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		factory.setMessageConverter(new StringJsonMessageConverter());
+		return factory;
+	}
 
+	@Bean
+	public ConsumerFactory<Integer, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+	}
 
-@Bean
-public ConsumerFactory<String, String> consumerFactory() {
-    return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-}
-
-@Bean
-public Map<String, Object> consumerConfigs() {
-    Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9095");
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "server-group");
-    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
-    return props;
+	@Bean
+	public Map<String, Object> consumerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9095");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "server-group");
+		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+		return props;
+	}
 }
--- java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/KafkaConsumer/L2KafkaConsumer.java
+++ java/TradeDataMessages/src/main/java/com/redpine/TradeDataMessages/PriceData/KafkaConsumer/L2KafkaConsumer.java
@@ -1,9 +1,18 @@
+package com.redpine.TradeDataMessages.PriceData.KafkaConsumer;
 
-@KafkaListener(id = "L1InChannel",topics =   CommonContants.KAFKA_TOPIC.SERVER_IN_CHANNEL)
-public void consumeInMessage(@Payload String data,
-                                 @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                              @Header(KafkaHeaders.RECEIVED_PARTITION_ID) String partitionId,
-                              Acknowledgment ack) {
-    logger.info("[serverInMessage]data=" + data);
-    ack.acknowledge();
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public class L2KafkaConsumer {
+
+	@KafkaListener(id = "L2InChannel", topics = "L2Topic")
+	public void consumeInMessage(@Payload String data,
+			@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) String partitionId,
+			Acknowledgment ack) {
+		ack.acknowledge();
+	}
 }
